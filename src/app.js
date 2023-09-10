ec12ae4239515acb853f3f01e7cc94a268687e59
meenaFotovvat <meenafotovvat@gmail.com>
2023-09-10 13:44:49 +0330

    add forecast content with js to html

--- src/app.js
+++ src/app.js
@@ -74,6 +74,26 @@ function convertTocentigrade(event) {
   tempValue.innerHTML = centigradeValue;
 }
 
+function dispalyForecastSection() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastContent = "";
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
+  days.forEach((item) => {
+    forecastContent =
+      forecastContent +
+      `<div>
+        <div>${item}</div>
+            <img
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
+              alt=""
+              id="icon"
+            /><span></span>
+    <div><span class="weather-forecast-temp-max">18°</span> <span class="weather-forecast-temp-min">12°</span></div></div>`;
+  });
+  console.log(forecastContent);
+  forecastElement.innerHTML = forecastContent;
+}
+
 let form = document.querySelector("#search-city");
 form.addEventListener("submit", handleSubmit);
 
@@ -86,3 +106,4 @@ let centigradeLink = document.querySelector("#centigrade-link");
 centigradeLink.addEventListener("click", convertTocentigrade);
 
 search("Mashhad");
+dispalyForecastSection();
